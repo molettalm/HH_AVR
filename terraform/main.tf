4f2584cc6c8c5ca5e8e751998764895a963cf279
Lucas Moletta <amiralezer@gmail.com>
2024-05-20 20:44:26 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,7 +98,7 @@ resource "aws_ecs_task_definition" "app_task" {
   requires_compatibilities = ["FARGATE"] # use Fargate as the lauch type
   network_mode             = "awsvpc"    # add the awsvpc network mode as this is required for Fargate
   memory                   = 512         # Specify the memory the container requires
-  cpu                      = 10         # Specify the CPU the container requires
+  cpu                      = 256         # Specify the CPU the container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
