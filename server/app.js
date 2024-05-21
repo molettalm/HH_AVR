1982d130314568ae0f7f4b10c9db6d80846a582e
Lucas Moletta <amiralezer@gmail.com>
2024-05-20 22:17:48 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,6 +13,19 @@ const app = express();
 app.use(cors()); // Use cors middleware to enable CORS for all routes
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use('/feed', feedRoutes);
 app.use('/exercises', exercisesRouter);
 app.use('/infos', infoRouter);
