2d8fc813618e47d8265999d14660bde9c9a42246
Lucas Moletta <amiralezer@gmail.com>
2024-05-27 21:30:02 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,39 +16,33 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1" # The region where environment is going to be deployed # Use your own region here
+  region = "us-east-1"
 }
-
 
 resource "aws_ecr_repository" "app_ecr_repo" {
   name = "hh-repo"
   lifecycle {
-   prevent_destroy = true
- }
+    prevent_destroy = true
+  }
 }
 
 resource "aws_ecs_cluster" "my_cluster" {
-  name = "hh-cluster" # Name your cluster here
+  name = "hh-cluster"
 }
 
-# Provide a reference to your default VPC
-resource "aws_default_vpc" "default_vpc" {
-}
-
+resource "aws_default_vpc" "default_vpc" {}
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
   lifecycle {
-   prevent_destroy = true
- }
+    prevent_destroy = true
+  }
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
-
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
@@ -56,20 +50,17 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-
-   lifecycle {
-   prevent_destroy = true
- }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-
 resource "aws_ecs_task_definition" "app_task" {
-  family                   = "hh-family-task" # Name your task
-  container_definitions = jsonencode([
+  family                   = "hh-family-task"
+  container_definitions    = jsonencode([
     {
       name      = "hh-client"
       image     = "${aws_ecr_repository.app_ecr_repo.repository_url}:client"
@@ -77,7 +68,6 @@ resource "aws_ecs_task_definition" "app_task" {
       portMappings = [
         {
           containerPort = 80
-
         }
       ]
     },
@@ -88,67 +78,67 @@ resource "aws_ecs_task_definition" "app_task" {
       portMappings = [
         {
           containerPort = 8080
-     
         }
       ]
     }
   ])
-  requires_compatibilities = ["FARGATE"] # use Fargate as the lauch type
-  network_mode             = "awsvpc"    # add the awsvpc network mode as this is required for Fargate
-  memory                   = 512         # Specify the memory the container requires
-  cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
 }
 
-
-# Provide references to your default subnets
 resource "aws_default_subnet" "default_subnet_a" {
-  # Use your own region here but reference to subnet 1a
   availability_zone = "us-east-1a"
-
   depends_on = [
     aws_default_vpc.default_vpc
   ]
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
-  # Use your own region here but reference to subnet 1b
   availability_zone = "us-east-1b"
   depends_on = [
     aws_default_vpc.default_vpc
   ]
 }
 
-resource "aws_alb" "application_load_balancer" {
-  name               = "hh-alb" # Naming our load balancer
-  load_balancer_type = "application"
-  subnets = [ # Referencing the default subnets
-    "${aws_default_subnet.default_subnet_a.id}",
-    "${aws_default_subnet.default_subnet_b.id}"
-  ]
-  # Referencing the security group
-  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
-}
-
-# Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
-  name = "load_balancer_sg"
+  name        = "load_balancer_sg"
   description = "Security group for ALB and ECS service"
-  vpc_id = aws_default_vpc.default_vpc.id
+  vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing HTTP traffic in from all sources
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "hh-alb"
+  load_balancer_type = "application"
+  subnets            = [
+    aws_default_subnet.default_subnet_a.id,
+    aws_default_subnet.default_subnet_b.id
+  ]
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+}
+
+resource "aws_lb_target_group" "frontend_target_group" {
+  name        = "frontend-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id
 }
 
 resource "aws_lb_target_group" "backend_target_group" {
@@ -156,34 +146,26 @@ resource "aws_lb_target_group" "backend_target_group" {
   port        = 8080
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}"
-}
-
-resource "aws_lb_target_group" "target_group" {
-  name        = "target-group"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  vpc_id      = aws_default_vpc.default_vpc.id
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  load_balancer_arn = aws_alb.application_load_balancer.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+    target_group_arn = aws_lb_target_group.frontend_target_group.arn
   }
 }
 
 resource "aws_lb_listener_rule" "backend_listener_rule" {
-  listener_arn = "${aws_lb_listener.listener.arn}"
+  listener_arn = aws_lb_listener.listener.arn
   priority     = 2
 
   action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.backend_target_group.arn}"
+    target_group_arn = aws_lb_target_group.backend_target_group.arn
   }
 
   condition {
@@ -193,36 +175,10 @@ resource "aws_lb_listener_rule" "backend_listener_rule" {
   }
 }
 
-resource "aws_ecs_service" "app_service" {
-  name            = "hh-service"                             # Name the  service
-  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Reference the created Cluster
-  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 1 # Set up the number of containers to 3
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
-    container_name   = "hh-client"
-    container_port   = 80 # Specify the container port
-  }
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.backend_target_group.arn}"
-    container_name   = "hh-server"
-    container_port   = 8080
-  }
-
-  network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
-    assign_public_ip = true                                                # Provide the containers with public IPs
-    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
-  }
-}
-
 resource "aws_security_group" "service_security_group" {
-  name = "ecs_service_sg"
+  name        = "ecs_service_sg"
   description = "Security group for ECS service"
-  vpc_id = aws_default_vpc.default_vpc.id
+  vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
     from_port   = 80
@@ -246,7 +202,32 @@ resource "aws_security_group" "service_security_group" {
   }
 }
 
-#Log the load balancer app url
+resource "aws_ecs_service" "app_service" {
+  name            = "hh-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.frontend_target_group.arn
+    container_name   = "hh-client"
+    container_port   = 80
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend_target_group.arn
+    container_name   = "hh-server"
+    container_port   = 8080
+  }
+
+  network_configuration {
+    subnets          = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.service_security_group.id]
+  }
+}
+
 output "app_url" {
   value = aws_alb.application_load_balancer.dns_name
 }
