ccf93b326b6f702ab367aa852d3b4a1ba4732551
Lucas Moletta <amiralezer@gmail.com>
2024-05-28 18:52:53 -0300

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,20 +23,42 @@ resource "aws_security_group" "hh_ec2_security_group" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
 
-  ingress {
+  ingress =[{
     description      = "SSH from anywhere"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
+  },
+    {
+    description      = "HTTP from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-  }
+  },
+  {
+    description      = "HTTPS from anywhere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }]
+
+
+  egress =[{
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  },
+  {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }]
+
 }
 
 resource "aws_instance" "hh_ec2" {
