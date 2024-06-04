9cc526cf08550a8124311527b1d26fc200386881
Lucas Moletta <amiralezer@gmail.com>
2024-06-03 21:58:02 -0300

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,13 +36,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.hh_ec2_security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.hh_ec2_security_group.id
